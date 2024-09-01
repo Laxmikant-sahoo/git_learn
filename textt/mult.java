e95aea7f1841e8f679bd480d946019859df8847c
Laxmikant Sahoo <laxmikantsahoo2003@gmail.com>
2024-09-01 23:59:17 +0530

    mutiplication happen

--- textt/mult.java
+++ textt/mult.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AddThreeNumbers {
+public class mult  {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             // Input three numbers
